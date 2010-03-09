4a59d9d47babdf180515c24c669838947ab7fb80
Stephane Angel <twidi@home.twidi.com>
2010-03-09 15:07:34 +0100

    adding setter (same logic as the getter), based on patch on http://code.google.com/p/django-transmeta/issues/detail?id=15
    Conflicts:
    
    	transmeta/__init__.py

--- transmeta/__init__.py
+++ transmeta/__init__.py
@@ -58,14 +58,14 @@ def get_all_translatable_fields(model, model_trans_fields=None, column_in_curren
     return tuple(model_trans_fields)
 
 
-def default_value(field):
+def default_value_getter(field):
     '''
     When accessing to the name of the field itself, the value
     in the current language will be returned. Unless it's set,
     the value in the default language will be returned.
     '''
 
-    def default_value_func(self):
+    def default_value_func_getter(self):
         attname = lambda x: get_real_fieldname(field, x)
 
         if getattr(self, attname(get_language()), None):
@@ -77,7 +77,27 @@ def default_value(field):
             result = getattr(self, attname(default_language), None)
         return result
 
-    return default_value_func
+    return default_value_func_getter
+
+
+def default_value_setter(field):
+    '''
+    When setting to the name of the field itself, the value
+    in the current language will be set.
+    '''
+
+    def default_value_func_setter(self, value):
+        attname = lambda x: get_real_fieldname(field, x)
+
+        if getattr(self, attname(get_language()), None):
+            setattr(self, attname(get_language()), value )
+        elif getattr(self, attname(get_language()[:2]), None):
+            setattr(self, attname(get_language()[:2]), value)
+        else:
+            default_language = fallback_language()
+            setattr(self, attname(default_language), value)
+
+    return default_value_func_setter
 
 
 class TransMeta(models.base.ModelBase):
@@ -145,7 +165,7 @@ class TransMeta(models.base.ModelBase):
                     lang_attr.verbose_name = LazyString(lang_attr.verbose_name, lang_code)
                 attrs[lang_attr_name] = lang_attr
             del attrs[field]
-            attrs[field] = property(default_value(field))
+            attrs[field] = property(default_value_getter(field), default_value_setter(field))
 
         new_class = super(TransMeta, cls).__new__(cls, name, bases, attrs)
         if hasattr(new_class, '_meta'):
